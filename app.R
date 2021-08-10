ebdc06ad257a2ee6b1dfe84539ef608e1b1f976c
lwolfe <lwolfe@fredhutch.org>
2021-08-10 15:36:43 -0700

    working app: set pval/logfc thresholds for de table, select axes cols for volcano

--- app.R
+++ app.R
@@ -7,42 +7,91 @@ library(plotly)
 source("volcano.R")
 
 # read in data
-data <- read.table("./data/210709_KO_vs_WT.txt", sep = "\t", header = T)
+data <- read.table("210709_KO_vs_WT.txt", sep = "\t", header = T)
+
+# functions
+pvalue_candidate_f <- function(x) {
+  if (class(data[[x]]) == "numeric") {
+    if (max(data[[x]]) <= 1) {
+      if (min(data[[x]]) >= 0) {
+        return(TRUE)
+      }
+    }
+  }
+  return(FALSE)
+}
+
+logfc_candidate_f <- function(x) {
+  if (class(data[[x]]) == "numeric") {
+        return(TRUE)}
+  return(FALSE)
+}
+
+gene_candidate_f <- function(x) {
+  if (class(data[[x]]) == "character") {
+    return(TRUE)}
+  return(FALSE)
+}
+
+# check data columns to ID most likely candidates for pval, logfc, and gene ID
+pval_cols <- names(data)[sapply(names(data), pvalue_candidate_f)]
+logfc_cols <- names(data)[sapply(names(data), logfc_candidate_f)]
+gene_cols <- names(data)[sapply(names(data), gene_candidate_f)]
 
 # ui -----------------------------
 ui <- fluidPage(
-  # title
-  #titlePanel("Data Core Interactive Volcano App"),
   # main panel
   mainPanel(
     # tab bar on main panel
     tabsetPanel(
                 tabPanel("Splash page",
                          h3("welcome to the carousel volcano plot app, 
-                           info about how this visualizaiton is created, where to find code, etc")),
-                # Data panel
-                tabPanel("Data",
-                         h1("Provided dataset"),
-                         sidebarLayout(
-                           sidebarPanel( width = 3,
-                                         checkboxInput("show_de",
-                                                       "Show only differentially expressed genes",
-                                                       FALSE)),
-                           mainPanel(dataTableOutput("gene_data")))),
-                         
+                           info about how this visualizaiton is created, where to find code, etc")), # end tabPanel
+                
                 # Volcano plot panel
                 tabPanel("Volcano Plot",
                          h1("Interactive Volcano Plot"),
                          sidebarLayout(
                            sidebarPanel(width = 3,
-                                        selectInput("gene_list",
-                                                    "Highlight gene(s)",
-                                                    sort(data[[gene_col]]),
-                                                    multiple = TRUE,
-                                                    selectize= TRUE)),
+                                        # select column for pval
+                                        selectInput("pvalue_col",
+                                                    "Select input column for P value",
+                                                    pval_cols,
+                                                    multiple = FALSE),
+                                        # select column for fold change
+                                        selectInput("logfc_col",
+                                                    "Select input column for fold change",
+                                                    logfc_cols,
+                                                    multiple = FALSE),
+                                        # select column for gene ID
+                                        selectInput("gene_col",
+                                                    "Select input column for gene ID",
+                                                    gene_cols,
+                                                    multiple = FALSE),
+                                        # set pvalue threshold 
+                                        sliderInput("pvalue_threshold",
+                                                    "Select P value / FDR threshold",
+                                                    min = 0,
+                                                    max = 1,
+                                                    value = .05),
+                                        # set logfc threshold
+                                        uiOutput("logfc_slider")),
+                                        # select genes to highlight
+                                        #uiOutput("gene_selector")),
                          mainPanel(plotOutput("volcano_plot",
                                       click = "volcano_click"),
-                                   verbatimTextOutput("info"))))
+                                   verbatimTextOutput("info"))
+                         ) # end sidebarLayout
+                         ), # end tabPanel
+                # Data panel
+                tabPanel("Data",
+                         h1("Provided dataset"),
+                         sidebarLayout(
+                           sidebarPanel(width = 3,
+                                        checkboxInput("show_de",
+                                                       "Show only differentially expressed genes",
+                                                       FALSE)),
+                           mainPanel(dataTableOutput("gene_data"))))
                 ) # end tabsetPanel
     ) # end mainPanel
 ) # end fluidPage
@@ -50,23 +99,53 @@ ui <- fluidPage(
 # server -------------------------
 server <- function(input, output) {
   
-  # filter dataframe based on checkbox
+  # reactively mark DE genes from using pval and logfc
+  is_de <- reactive({
+    abs(data[[input$logfc_col]]) >= input$logfc_threshold & data[[input$pvalue_col]] <= input$pvalue_threshold
+  })
+  
+  # reactively filter dataframe based on checkbox
   filtered_gene_data <- reactive({
     if (input$show_de) {
-      filter(data, is.DE != 0)
+      filter(data, is_de())
     } else {
       data
     }
   })
   
-  # output dataframe
+  # render data frame of gene data
   output$gene_data <- renderDataTable(
     filtered_gene_data()
   )
-
+  
+  # render UI for logfc slider
+  # min and max set reactively with logfc based on selected logfc input col
+  output$logfc_slider <- renderUI({
+    sliderInput("logfc_threshold",
+                "Select log fold change threshold",
+                min = 0,
+                max = round(max(data[[input$logfc_col]])),
+                value = 2)
+  })
+  
+  # # gene
+  output$gene_selector <- renderUI({
+    selectInput("selected_genes",
+                "Highlight gene(s)",
+                sort(data[[input$gene_col]]),
+                multiple = TRUE,
+                selectize= TRUE)
+  })
+  
   # output volcano plot
   output$volcano_plot <- renderPlot({
-    plotVolcano(data, "logFC", "FDR", "Genename")
+    plotVolcano(data = data, 
+                logfc_col = input$logfc_col, 
+                pval_col = input$pvalue_col,
+                gene_col = input$gene_col,
+                pval_thresh = .05,
+                logfc_thresh = 2)
+                
   })
   
   # output volcano_click info as text  
