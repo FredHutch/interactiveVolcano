cab500b858dc700a5ee302bae31460c52a23b3ba
lwolfe <lwolfe@fredhutch.org>
2021-08-10 15:37:16 -0700

    add more params for reactivity

--- volcano.R
+++ volcano.R
@@ -1,28 +1,35 @@
 ######
-# data <- read.table("./data/210709_KO_vs_WT.txt", sep = "\t", header = T)
-log_col <- "logFC"
-fdr_col <- "FDR"
-gene_col <- "Genename"
 
+
+# pval_thresh <- .05
+# logfc_thresh <- 2
+# de_vec <- 
 ######
+
+
 
 ## NOTES ##
 # - hardcoding is.DE for now because it is already generated. Working version will take
 #   p value and lfc considered significant as an argument and create is.DE column.
 
-plotVolcano <- function(data, log_col, fdr_col, gene_col) {
+plotVolcano <- function(data, 
+                        logfc_col, 
+                        pval_col, 
+                        gene_col, 
+                        pval_thresh, 
+                        logfc_thresh,
+                        de_vec) {
   # check that columns exist
-  if (!all(c(log_col, fdr_col, gene_col) %in% colnames(data))) {
+  if (!all(c(logfc_col, pval_col, gene_col) %in% colnames(data))) {
     stop("provided column names do not match dataset")
   }
   # convert pval to -log10(pval)
   data <- mutate(data,
-         log_fdr = -log10(.data[[fdr_col]]),
-         is.DE_fac = as.factor(is.DE))
+                 log_pval = -log10(data[[pval_col]]))
   # build plot
-  ggplot(data, aes(x = .data[[log_col]], y = log_fdr, color = is.DE_fac)) +
-    geom_point(alpha = .7)+
-    geom_hline(yintercept = -log10(0.01), col = "red") +
+  ggplot(data, aes(x = .data[[logfc_col]], y = log_pval)) +
+    geom_point(alpha = .7) +
+    geom_hline(yintercept = -log10(pval_thresh), col = "red") +
+    geom_vline(xintercept = c(logfc_thresh, -logfc_thresh), col = "red") +
     theme_minimal()
-  #ggplotly(v, tooltip = "Genename")
 }
